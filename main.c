323de8cf414daece69e008bf1ebae435e1d46ce2
lilly5791 <1913084@sookmyung.ac.kr>
2019-11-21 23:18:38 +0900

    REALLY FINAL END

--- main.c
+++ main.c
@@ -5,44 +5,6 @@
 #include "card array controllers.h"
 #include "playing game functions.h"
 
-
-//#define N_CARDSET         1
-//#define N_CARD            52
-//#define N_DOLLAR         50
-//
-//#define N_MAX_CARDNUM      13
-//#define N_MAX_USER         5
-//#define N_MAX_CARDHOLD      10
-//#define N_MAX_GO         17
-//#define N_MAX_BET         5
-//
-//#define N_MIN_ENDCARD      30
-
-
-//card tray object
-//int CardTray[N_CARDSET*N_CARD]; //[1*52]
-//int cardIndex = 0;
-//int pullcardIndex=0;
-
-////player info
-//int dollar[N_MAX_USER];                  //dollars that each player has
-//int n_user;                           //number of users
-//int playercardhold[N_MAX_USER+1] = {2,2,2,2,2,2};            //card number of each player
-//
-////play yard information
-//int cardhold[N_MAX_USER+1][N_MAX_CARDHOLD];   //cards that currently the players hold, [6][10]
-//int cardSum[N_MAX_USER+1];               //sum of the cards [5]
-//int bet[N_MAX_USER];                  //current betting  [5]
-//int gameEnd = 0;                      //game end flag
-
-//some utility functions
-
-//get an integer input from standard input (keyboard)
-//return : input integer value
-//         (-1 is returned if keyboard input was not integer)
-
-
-
 int main(int argc, char *argv[]) {
    int roundIndex = 1;
    int i; 
